f4de1b1b988791b4281ec24b2f56a7161386fd2f
CriskunVN <phukun0404@gmail.com>
2025-04-04 16:08:23 +0700

    chapter 8-#14

--- controller/tourController.js
+++ controller/tourController.js
@@ -2,8 +2,17 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // Build Query
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
 
+    const query = Tour.find(queryObj);
+
+    // Execute Query
+    const tours = await query;
+
+    // Send response to client
     res.status(200).json({
       status: 'success',
       result: tours.length,
---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,51 @@
+// 4. SERVER
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+const Tour = require('./../../models/tourModel');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log('DB connection successful!'));
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
+);
+
+//Import data
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+//Delete data
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully delete');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
