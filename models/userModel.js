762584a8a50d46e06d2ec84689f1c063045b8017
CriskunVN <phukun0404@gmail.com>
2025-05-05 23:22:00 +0700

    Chapter 10-003 create controller user and route

---
+++ models/userModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const { isLowercase } = require('validator');
+const validator = require('validator');
+
+// name , email , password , passwordConfirm , photo , role , active
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A user must have a name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'A user must have a email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  password: {
+    type: String,
+    required: [true, 'A user must have a password'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'A user must have a passwordConfirm'],
+    validate: {
+      // This only works on CREATE and SAVE!!!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Password are not the same!',
+    },
+  },
+  photo: String,
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,7 +6,13 @@ const {
   updateUserById,
   deleteUserById,
 } = require('../controller/userController');
+
+const { signup } = require('../controller/authController');
+
 const router = express.Router();
+
+// authentication
+router.post('/signup', signup);
 
 // users
 router.route('/').get(getAllUsers).post(createUser);
