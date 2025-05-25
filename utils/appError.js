bb5c42a53b25786f9b9c610d8a7a6c424c867719
CriskunVN <phukun0404@gmail.com>
2025-05-26 00:00:00 +0700

    add comment code v4

--- utils/appError.js
+++ utils/appError.js
@@ -2,9 +2,10 @@ class AppError extends Error {
   constructor(message, statusCode) {
     super(message);
     this.statusCode = statusCode;
-    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
-    this.isOperational = true;
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error'; // Set the status to 'fail' for 4xx errors and 'error' for 5xx errors
+    this.isOperational = true; // Indicate that this is an operational error
 
+    // Capture the stack trace for better debugging
     Error.captureStackTrace(this, this.constructor);
   }
 }
--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -1,3 +1,5 @@
+// Description: A utility function to catch errors in async route handlers
+// This function wraps an async function and catches any errors that occur,
 module.exports = (fn) => {
   // fn is a function that returns a promise
   return (req, res, next) => {
